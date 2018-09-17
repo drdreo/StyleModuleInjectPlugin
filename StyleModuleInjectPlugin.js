597b2fba70dabe4cccbce4bd9aaf7ec4e37d8179
Andreas Hahn <drdreo@users.noreply.github.com>
2018-09-17 12:46:07 +0200

    Update StyleModuleInjectPlugin.js

--- StyleModuleInjectPlugin.js
+++ StyleModuleInjectPlugin.js
@@ -6,26 +6,31 @@
  * A big thanks to Jose A. who created a similar task for gulp.
  * He explained it in detail at stackoverflow: https://stackoverflow.com/a/37666406/6699493
  *
- * Description: A Webpack plugin or standalone class to convert sass to css and inject it into a polymer style module.
+ * Description: A Webpack plugin or standalone class to convert sass to css and inject it into a polymer 3 style module.
  * Usage:
  *  let plugin = new StyleModuleInjectPlugin({ options });
 		plugin.convertAndInject();
 
 		available options: {
-					styleFolder: './scss', 					directory that the plugin should check for styles										
-					moduleFolder: './style-modules',			directory that the plugin should check for modules
-					extension: /\.scss$/,					file extension to look for
-					includePaths: ["src/webcomponents/style-modules"], 	https://www.npmjs.com/package/node-sass#includepaths
-					outputStyle: 'nested', 					https://www.npmjs.com/package/node-sass#outputstyle
-					cssFolder: './css',					directory where compiled css files will be written to
-					webpackHook: 'run' 					https://webpack.js.org/api/compiler-hooks/
-					startComment: "/*inject_start{scss}<star>/",		defines the start point of injection
-					endComment: "/*inject_end{scss}<star>/"			defines the end point of injection
-			 	  }
+						polymerVersion: 2																		the polymer version used, optional - sets the right style module extension
+						styleFolder: './scss',															directory that the plugin should check for styles										
+						moduleFolder: './style-modules',										directory that the plugin should check for modules
+						extension: /\.scss$/,																file extension to look for
+						includePaths: ["src/webcomponents/style-modules"], 	https://www.npmjs.com/package/node-sass#includepaths, optional
+						outputStyle: 'nested', 															https://www.npmjs.com/package/node-sass#outputstyle, optional
+						cssFolder: './css',																	the directory where the css files should be written to, optional
+						webpackHook: 'run' 																	https://webpack.js.org/api/compiler-hooks/, optional
+						startComment: "/*inject_start{scss}<star>/",				defines the start point of injection, optional
+						endComment: "/*inject_end{scss}<star>/"							defines the end point of injection, optional
+				 }
  */
 const fs = require('fs');
 const path = require('path');
 const nodeSass = require('node-sass');
+const chalk = require('chalk');
+var autoprefixer = require('autoprefixer');
+var postcss = require('postcss');
+const postcssUrl = require('postcss-url');
 
 class StyleModuleInjectPlugin {
 
@@ -45,11 +50,21 @@ class StyleModuleInjectPlugin {
 			throw { name: "MissingArgumentError", message: "Extension option is missing!" };
 		}
 
+		// Polymer 2 used style modules as HTML, 
+		// in version 3, they switched to JavaScript files
+		this.moduleExtension = ".html";
+		if (Math.round(this.options.polymerVersion) == 3) {
+			this.moduleExtension = ".js";
+		}
+		
 		this.startStyle = this.options.startComment || "/*inject_start{scss}*/";
 		this.endStyle = this.options.endComment || "/*inject_end{scss}*/";
 
 		//This will match anything between the Start and End Style, so we can reinject and overwrite again and again.
 		this.regExp = new RegExp(this.escapeRegExp(this.startStyle) + "[\\s\\S]*" + this.escapeRegExp(this.endStyle), "g");
+
+		// counts all processed files
+		this.cntProcessedFiles = 0;
 	}
 
 	// thanks to https://stackoverflow.com/a/25462405/6699493
@@ -63,6 +78,7 @@ class StyleModuleInjectPlugin {
 		let files = fs.readdirSync(startPath);
 
 		for (let i = 0; i < files.length; i++) {
+
 			let file = {
 				path: path.join(startPath, files[i]),
 				name: files[i]
@@ -71,103 +87,132 @@ class StyleModuleInjectPlugin {
 			if (stat.isDirectory()) {
 				this.ReadFilesfromDir(file.path, filter, callback); //recurse
 			} else if (filter.test(file.path)) {
+
+				this.cntProcessedFiles++;
+
 				callback(file);
 			}
-		};
+		}
 	}
 
 	compileSass(file) {
 
 		let result = { };
 		try {
-
 			result = nodeSass.renderSync({
 				file: file,
 				includePaths: this.options.includePaths,
 				outputStyle: this.options.outputStyle
 			});
 		} catch (err) {
-			console.log('Couldn\'t compile the given file: ', err.formatted);
+			console.log(chalk.redBright('Couldn\'t compile the given file: ', err.formatted));
 		}
 		return result.css;
 	}
 
-	writeCssToFile(css,filepath, cb) {
+	writeCssToFile(css, filepath, cb) {
 		let cssContent = new Buffer(css, 'binary');
 
 		// write the content back to the module
 		fs.writeFile(filepath, cssContent, (err) => {
 			if (err) {
+				// probably missing write permissions
 				return cb(false);
 			}
 			return cb(true);
 		});
 	}
 
-	injectCSSinStyleModule(moduleFile, css, cb) {
+	injectCSSinStyleModule(moduleFile, css) {
 
 		// If there is no css
 		if (!css) {
-			return cb(null);
+			return new Promise(function(resolve, reject) {
+				reject(null);
+			});
 		}
 
 		let styleModuleContent = fs.readFileSync(moduleFile, "utf8");
 
 		// if the style_module is empty or the RegEx doesn't exists in the file, return null.
 		if (!styleModuleContent || !this.regExp.test(styleModuleContent)) {
-			return cb(null);
+			return new Promise(function(resolve, reject) {
+				reject(null);
+			});
 		}
 		/**
 		 * Re-create the start and end placeholders and inject them back to the style module
 		 * This will allow us to re-inject over and over again 
 		 */
-		const injectCssContent = this.startStyle + "\n/*Content between those comments is autogenerated.*/ " + css.toString() + this.endStyle;
+		const injectCssContent = this.startStyle + "\n/*Content between those comments is autogenerated.*/ \n" + css.toString() + this.endStyle;
 
 		// This is going to replace everything that was between the this.startStyle and this.endStyle
 		styleModuleContent = new Buffer(styleModuleContent.replace(this.regExp, injectCssContent), 'binary');
 
 		// write the content back to the module
-		fs.writeFile(moduleFile, styleModuleContent, (err) => {
-			if (err) {
-				return cb(false);
-			}
-			return cb(true);
+
+		return new Promise(function(resolve, reject) {
+			fs.writeFile(moduleFile, styleModuleContent, (err) => {
+				if (err) {
+					reject(false);
+				} else {
+					resolve(true);
+				}
+			});
 		});
 	}
 
 	convertAndInject() {
+
 		this.ReadFilesfromDir(this.options.styleFolder, this.options.extension, (sassfile) => {
 			// get the related style-module for the SASS file
-			const moduleFile = this.options.moduleFolder + "/" + sassfile.name.replace(this.options.extension, '.html');
+			const moduleFile = this.options.moduleFolder + "/" + sassfile.name.replace(this.options.extension, this.moduleExtension);
+			const originFile = sassfile.path;
 
-			console.log("Module file:", moduleFile);
 			// only compile and inject if the module exists
 			if (fs.existsSync(moduleFile)) {
-
-				// TODO : SCSS Lint 
 
 				// compile the given sass file to css
 				let compiledCSS = this.compileSass(sassfile.path);
 
-				// write the compiled css to a file if option.cssFolder is given
-				if (!this.options.cssFolder) {
-					const cssFile = this.options.cssFolder + "/" + sassfile.name.replace(this.options.extension, '.css');
+				postcss([autoprefixer])
+					.use(postcssUrl({ url: "rebase" }))
+					.process(compiledCSS, { from: originFile, to: moduleFile })
+					.then((result) => {
 
-					this.writeCssToFile(compiledCSS,cssFile,(success) => {
-						
+						result.warnings().forEach(function(warn) {
+							console.log(warn.toString());
+						});
+
+						// console.log(result.css);
+						compiledCSS = result.css;
+
+						// write the compiled css to a file if option.cssFolder is given
+						if (!this.options.cssFolder) {
+							const cssFile = this.options.cssFolder + "/" + sassfile.name.replace(this.options.extension, '.css');
+
+							this.writeCssToFile(compiledCSS, cssFile, (success) => {
+								// todo  handle file write errors
+							});
+						}
+
+						return this.injectCSSinStyleModule(moduleFile, compiledCSS);
+					})
+					.then((success) => {
+
+						if (success == true) {
+							console.log(chalk.green("[SMIPlugin]      Injected " + originFile + "\tinto: \t\t", moduleFile));
+						}
+
+						// handle stuff after injection
+
+					}).catch((error) => {
+						if (error == null) {
+							console.log(chalk.yellow("[SMIPlugin][WAR] Tried to inject to: " + moduleFile + " but no injection comments found!"));
+						} else if (error == false) {
+							console.log(chalk.redBright("[SMIPlugin][ERR] Couldn't write to file!", moduleFile));
+						}
 					});
-				}
-
-				// TODO  -> PostCSS / Autoprefixer
-
-				this.injectCSSinStyleModule(moduleFile, compiledCSS, (success) => {
-					if (success == null) {
-						console.log("[WAR] Tried to inject to: " + moduleFile + " but no injection comments found!");
-					} else {
-						console.log(success + " " + moduleFile);
-					}
-					// handle stuff after injection
-				});
 			}
 		});
 	}
